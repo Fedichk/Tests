16942369d5482c2204ac3c64a6d8da5cd1233386
Fedir kantur <fedichk@gmail.com>
2018-08-22 21:57:54 +0300

    Added crutches

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TagFilter;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
@@ -8,24 +8,30 @@ import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 
 public class MyClass {
 
     public static void main(String[] args) {
+
+        String[] filters = {"syntax", "functionality"};
+        if (args.length > 0) {
+            filters = args;
+        }
+
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
                 .selectors(
-                        selectPackage("com.tests.src.test.java"),
-                        selectClass(MyClass.class))
+                        selectClass("GetTest"),
+                        selectClass("PostTest"))
+                .filters(
+                        TagFilter.includeTags(filters)
+                )
                 .build();
 
         Launcher launcher = LauncherFactory.create();
         SummaryGeneratingListener listener = new SummaryGeneratingListener();
         launcher.registerTestExecutionListeners(listener);
-
         launcher.execute(request);
 
         TestExecutionSummary summary = listener.getSummary();
@@ -34,10 +40,5 @@ public class MyClass {
         System.out.println("getTestsSucceededCount() - " + summary.getTestsSucceededCount());
         failures.forEach(failure -> System.out.println("failure - " + failure.getException()));
         System.out.println("Founding tests: " + testFoundCount);
-    }
-
-    @Test
-    void exceptionTesting() {
-        assertEquals("hello", "hello");
     }
 }
