5ebca07e942ef629f172ddb6a00e71b1efa62625
Fedir kantur <fedichk@gmail.com>
2018-08-22 20:53:17 +0300

    Update

--- src/test/java/MyClass.java
+++ src/test/java/MyClass.java
@@ -1,24 +1,43 @@
+import org.junit.jupiter.api.Test;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
-import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 
 public class MyClass {
 
     public static void main(String[] args) {
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
-                .selectors(selectClass(MyClass.class))
+                .selectors(
+                        selectPackage("com.tests.src.test.java"),
+                        selectClass(MyClass.class))
                 .build();
 
         Launcher launcher = LauncherFactory.create();
-        TestExecutionListener listener = new SummaryGeneratingListener();
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
         launcher.registerTestExecutionListeners(listener);
 
         launcher.execute(request);
+
+        TestExecutionSummary summary = listener.getSummary();
+        long testFoundCount = summary.getTestsFoundCount();
+        List<TestExecutionSummary.Failure> failures = summary.getFailures();
+        System.out.println("getTestsSucceededCount() - " + summary.getTestsSucceededCount());
+        failures.forEach(failure -> System.out.println("failure - " + failure.getException()));
+        System.out.println("Founding tests: " + testFoundCount);
+    }
+
+    @Test
+    void exceptionTesting() {
+        assertEquals("hello", "hello");
     }
 }
